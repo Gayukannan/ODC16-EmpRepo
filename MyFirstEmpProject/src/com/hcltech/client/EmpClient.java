d791cfc56ea0f7df356991ab5340bcd293473719
Janani Shanmugi M A <janani.shanmugima@hcltech.com>
2025-07-19 12:45:21 +0530

    added validation code

--- MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
+++ MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
@@ -5,7 +5,7 @@ public class EmpClient {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Emp project is done");
-
+		System.out.println("line 2");
 	}
 
 }
